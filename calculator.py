6532dbf0fe190dc582e860586923520a5379bf77
Subha Mannargudi <subha.ms@gmail.com>
2020-09-30 23:32:43 +0000

    Calculator added valuations

--- calculator.py
+++ calculator.py
@@ -13,14 +13,14 @@ while True:
     if user_tokens[0]  == 'q':
         break
 
-    elif len(user_tokens) == 2:
+    elif len(user_tokens) == 2 and user_tokens[1].isdigit() == True:
         num1 = user_tokens[1]
         operator = user_tokens[0]
         if operator == 'square':
             output = square(float(num1))
         elif operator =='cube':
             output = cube(float(num1))
-    else:
+    elif len(user_tokens) == 3 and user_tokens[1].isdigit() and user_tokens[2].isdigit():
         operator = user_tokens[0]
         num1 = user_tokens[1]
         num2 = user_tokens[2]
@@ -38,5 +38,7 @@ while True:
             output = power(float(num1), float(num2))
         elif operator == 'mod':
             output = mod(float(num1), float(num2))
+    else:
+        output = 'Error'
 
     print(output)
